b8278be003a3c48dd0a8b3ec25216a185f731812
tinkermanz <m.b.istiak@outlook.com>
2025-06-16 12:27:07 +0600

    feat(user): add GET /user/connections endpoint to fetch user connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -31,4 +31,45 @@ userRouter.get("/user/requests/received", userAuth, async (req, res, next) => {
 	}
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res, next) => {
+	try {
+		const loggedInUser = req.user;
+
+		const connectionRequests = await ConnectionRequest.find({
+			$or: [
+				{
+					toUserId: loggedInUser._id,
+					status: "accepted",
+				},
+				{
+					fromUserId: loggedInUser._id,
+					status: "accepted",
+				},
+			],
+		})
+			.populate(
+				"fromUserId",
+				"firstname lastName age gender about photUrl skills"
+			)
+			.populate(
+				"fromUserId",
+				"firstname lastName age gender about photUrl skills"
+			);
+
+		const data = connectionRequests.map((row) => {
+			if (row.fromUserId._id.toString() === loggedInUser._id.toString())
+				return row.toUserId;
+			return row.toUserId;
+		});
+
+		res.json({
+			data,
+		});
+	} catch (error) {
+		res.status(400).send({
+			message: error.message,
+		});
+	}
+});
+
 export { userRouter };
