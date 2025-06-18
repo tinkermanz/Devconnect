f1235c12536d4793eff0a12434135e9f300af53b
tinkermanz <m.b.istiak@outlook.com>
2025-06-18 19:49:33 +0600

    feat(routes): add initial routes and skeleton components for Body, Footer, Login, and Profile pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
 	return (
 		<>
-			<Navbar />
+			<BrowserRouter basename="/">
+				<Routes>
+					<Route path="/" element={<Body />}>
+						<Route path="login" element={<Login />} />
+						<Route path="profile" element={<Profile />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+	return <div className="min-h-screen">Login Page</div>;
+};
+
+export default Login;
